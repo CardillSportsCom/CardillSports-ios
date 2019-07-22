da6d21b49ab6f46743e9e4514a818cb2a29f461c
vithushan <vithushan19@gmail.com>
2019-07-21 21:17:41 -0400

    Initial Commit

---
+++ stattracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  stattracker
+//
+//  Created by Vithushan Namasivayasivam on 7/21/19.
+//  Copyright © 2019 CardillSports. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ stattracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  stattracker
+//
+//  Created by Vithushan Namasivayasivam on 7/21/19.
+//  Copyright © 2019 CardillSports. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
